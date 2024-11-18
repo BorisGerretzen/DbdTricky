1f84fb9bc4194f90311cf0f8d2f2489243a93823
Boris <borisgerretzen@gmail.com>
2024-11-18 22:16:06 +0100

    Remove comparison logic from version

--- src/DbdTrickyVersionChecker/Program.cs
+++ src/DbdTrickyVersionChecker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DbdTricky.Lib.Common;
 using DbdTricky.Lib.Versions;
+using DbdTrickyVersionChecker;
 
 var configuration = new DbdTrickyConfiguration();
 var http = new HttpClient
@@ -16,12 +17,13 @@ var current = await versionsClient.GetVersions();
 var lastKnown = DbdTrickyVersions.LastKnown;
 
 var properties = typeof(DbdTrickyVersions).GetProperties();
+var comparer = new VersionComparer();
 foreach (var property in properties)
 {
     var currentVersion = (DbdTrickyVersion?)property.GetValue(current) ?? throw new InvalidOperationException($"Current version for property {property.Name} is null.");
     var lastKnownVersion = (DbdTrickyVersion?)property.GetValue(lastKnown) ?? throw new InvalidOperationException($"Last known version for property {property.Name} is null.");
 
-    if (currentVersion > lastKnownVersion)
+    if (comparer.Compare(currentVersion, lastKnownVersion) > 0)
     {
         Console.WriteLine($"New version detected for {property.Name}: {currentVersion.Version} (last known: {lastKnownVersion.Version})");
     }
---
+++ src/DbdTrickyVersionChecker/VersionComparer.cs
@@ -0,0 +1,23 @@
+﻿using DbdTricky.Lib.Versions;
+
+namespace DbdTrickyVersionChecker;
+
+public class VersionComparer : IComparer<DbdTrickyVersion>
+{
+    public int Compare(DbdTrickyVersion? x, DbdTrickyVersion? y)
+    {
+        if (ReferenceEquals(x, y)) return 0;
+        if (x is null) return 1;
+        if (y is null) return -1;
+
+        foreach (var pair in x.Version.Split('.').Zip(y.Version.Split('.')))
+        {
+            var left = int.Parse(pair.First);
+            var right = int.Parse(pair.Second);
+            var comparison = left.CompareTo(right);
+            if (comparison != 0) return comparison;
+        }
+        
+        return x.LastUpdate.CompareTo(y.LastUpdate);
+    }
+}
