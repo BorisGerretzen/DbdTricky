378d8135ae2b4becda4f8cd5ce23d9ffeddc208f
Boris <borisgerretzen@gmail.com>
2024-11-18 12:41:16 +0100

    CI

--- src/DbdTricky.Tests/Infrastructure/TestHttpClientFactory.cs
+++ src/DbdTricky.Tests/Infrastructure/TestHttpClientFactory.cs
@@ -6,12 +6,14 @@ namespace DbdTricky.Tests.Infrastructure;
 
 public static class TestHttpClientFactory
 {
+    private const string BaseUrl = "https://dbd.tricky.lol/api";
+    
     public static HttpClient CreateClient(string url, string? content = null, HttpStatusCode statusCode = HttpStatusCode.OK)
     {
         var handler = new MockHttpMessageHandler();
         
-        if(content == null) handler.When(url).Respond(statusCode);
-        else handler.When(url).Respond(statusCode, "application/json", content);
+        if(content == null) handler.When(BaseUrl + url).Respond(statusCode);
+        else handler.When(BaseUrl + url).Respond(statusCode, "application/json", content);
         
         var client = new HttpClient(handler);
         var configuration = new DbdTrickyConfiguration();
